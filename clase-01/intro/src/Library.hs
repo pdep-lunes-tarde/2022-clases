df57edc072313d570d0d0dd9917d5f550a51a994
juanFdS <juan9794@gmail.com>
2022-03-28 02:40:35 -0300

    Agregado codigo para guiar clase 1

--- clase-01/intro/src/Library.hs
+++ clase-01/intro/src/Library.hs
@@ -3,3 +3,15 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+perimetroCirculo = implementame
+
+perimetroCuadrado = implementame
+
+superficieCuadrado = implementame
+
+superficieCubo = implementame
+
+superficieCilindro = implementame
+
+superficieCirculo = implementame

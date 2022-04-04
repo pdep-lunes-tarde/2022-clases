57d1828b871ce45c8b3b4cf9d4b927743ab94b52
juanFdS <juan9794@gmail.com>
2022-04-04 16:01:32 -0300

    Subido codigo escrito en clase 2

--- clase-02/src/Library.hs
+++ clase-02/src/Library.hs
@@ -2,8 +2,36 @@ module Library where
 import PdePreludat
 import Data.List (isPrefixOf, isSuffixOf)
 
+-- Tomas
+-- Carolina
+-- Joaquin
+-- Manuel
+-- Juani
+-- Franco
+-- Martin
+-- Matias
+
+-- versión refactorizada:
 preguntar :: String -> String
-preguntar oracion = implementame
+preguntar oracion = agregarSignoDePreguntaQueAbre (agregarSignoDePreguntaQueCierra oracion)
+
+agregarSignoDePreguntaQueCierra :: String -> String
+agregarSignoDePreguntaQueCierra oracion
+    | not (isSuffixOf "?" oracion) = oracion ++ "?"
+    | otherwise = oracion
+
+agregarSignoDePreguntaQueAbre :: String -> String 
+agregarSignoDePreguntaQueAbre oracion
+    | not (isPrefixOf  "¿" oracion) = "¿" ++ oracion
+    | otherwise = oracion
+
+-- versión sin refactorizar:
+preguntar' :: String -> String
+preguntar' oracion 
+    | isSuffixOf "?" oracion && isPrefixOf "¿" oracion = oracion
+    | not (isSuffixOf "?" oracion) && not (isPrefixOf  "¿" oracion) = "¿" ++ oracion ++ "?"
+    | not (isSuffixOf "?" oracion) = oracion ++ "?"
+    | otherwise = "¿" ++ oracion
 
 -- isPrefixOf nos dice si un string empieza con otro string
 -- isSuffixOf nos dice si un string termina con otro string
@@ -20,6 +48,8 @@ preguntar oracion = implementame
 
 -- Queremos que funcione así:
 -- >>> preguntar "hola"
--- "hola?"
+-- "¿hola?"
 -- >>> preguntar "hola?"
--- "hola?"
+-- "¿hola?"
+-- >>> preguntar "¿hola?"
+-- "¿hola?"
--- clase-02/src/Spec.hs
+++ clase-02/src/Spec.hs
@@ -5,7 +5,16 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Tests de clase 2" $ do
-    it "test de prueba" $ do
-      1 + 1 `shouldBe` 2
+  describe "preguntar" $ do
+    it "si le paso una oracion que ya tiene signos de pregunta, la deja igual" $ do
+      preguntar "¿como estas?" `shouldBe` "¿como estas?"
+      preguntar "¿todo bien?" `shouldBe` "¿todo bien?"
+    it "si le paso una oracion que le falta el signo de pregunta que cierra, se lo agrega" $ do
+      preguntar "¿como estas" `shouldBe` "¿como estas?"
+      preguntar "¿hay alguien" `shouldBe` "¿hay alguien?"
+    it "si le paso una oracion que le falta el signo de pregunta que abre, se lo agrega" $ do
+      preguntar "como estas?" `shouldBe` "¿como estas?"
+      preguntar "todo bien?" `shouldBe` "¿todo bien?"
+    it "si le paso una oracion sin ningun signo de pregunta, agrega ambos" $ do
+      preguntar "hola" `shouldBe` "¿hola?"
 

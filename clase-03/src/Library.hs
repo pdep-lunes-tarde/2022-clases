68608cef00e91f667d6b9905422480b13750fbd9
juanFdS <juan9794@gmail.com>
2022-04-11 17:09:09 -0300

    Codigo clase 3

--- clase-03/src/Library.hs
+++ clase-03/src/Library.hs
@@ -1,6 +1,81 @@
 module Library where
 import PdePreludat
 
+ochoAzul :: Carta
+ochoAzul = CartaNumerica 8 Azul
+
+-- type Carta = (Number, Color)
+
+-- data Valor =
+--     Uno |
+--     Dos |
+--     Tres |
+--     Cuatro | 
+--     Cinco |
+--     Seis |
+--     Siete |
+--     Ocho |
+--     Nueve |
+--     Cero |
+--     Reversa |
+--     SaltarTurno
+--     deriving (Eq, Show)
+-- data Carta = Carta Valor Color deriving (Eq, Show)
+
+
+data Simbolo = Reversa | Mas4 | SaltarTurno deriving (Eq, Show)
+-- False, True :: Bool
+data Color = Rojo | Amarillo | Verde | Azul deriving (Eq, Show)
+
+data Carta =
+    CartaNumerica { numero :: Number, color :: Color } |
+    CartaEspecial { simbolo :: Simbolo, color :: Color }
+    deriving (Eq, Show)
+
+-- instance Eq Carta where
+--     (CartaNumerica unNumero unColor) == (CartaNumerica otroNumero otroColor) = unNumero == otroNumero && unColor == otroColor
+    
+--     (CartaEspecial unSimbolo unColor) == (CartaEspecial otroSimbolo otroColor) = unSimbolo == otroSimbolo && unColor == otroColor
+    
+--     _ == _ = False
+    
+
+sePuedeJugar :: Carta -> Carta -> Bool
+-- sePuedeJugar _ (CartaEspecial simbolo _)
+--     | simbolo == Mas4 = True
+sePuedeJugar _ (CartaEspecial Mas4 _) = True
+sePuedeJugar unaCarta otraCarta =
+    tienenMismoValor unaCarta otraCarta ||
+    tienenMismoColor unaCarta otraCarta
+
+-- data Carta =
+--     CartaNumerica Number Color |
+--     CartaEspecial Simbolo Color
+--     deriving (Eq, Show)
+
+-- numero :: Carta -> Number
+-- numero (CartaNumerica unNumero _) = unNumero
+
+-- simbolo :: Carta -> Simbolo
+-- simbolo (CartaEspecial unSimbolo _) = unSimbolo
+
+-- color :: Carta -> Color
+-- color (CartaNumerica numero unColor) = unColor
+-- color (CartaEspecial simbolo unColor) = unColor
+
+
+tienenMismoValor :: Carta -> Carta -> Bool
+tienenMismoValor (CartaNumerica unNumero _) (CartaNumerica otroNumero _) = unNumero == otroNumero
+
+tienenMismoValor (CartaEspecial unSimbolo _) (CartaEspecial otroSimbolo _) = unSimbolo == otroSimbolo
+
+tienenMismoValor _ _ = False
+
+tienenMismoColor :: Carta -> Carta -> Bool
+tienenMismoColor unaCarta otraCarta = color unaCarta == color otraCarta
+
+-- fst :: (a, b) -> a
+
 -- Queremos saber si una carta del Uno se puede jugar luego de otra.
 
 -- Las cartas que tienen numero y color, se pueden jugar sobre cartas que tengan el mismo numero o el mismo color.
--- clase-03/src/Spec.hs
+++ clase-03/src/Spec.hs
@@ -3,8 +3,42 @@ import PdePreludat
 import Library
 import Test.Hspec
 
+ochoRojo = CartaNumerica 8 Rojo
+sieteAzul = CartaNumerica 7 Azul
+
+sieteVerde :: Carta
+sieteVerde = CartaNumerica 7 Verde
+
+reversaVerde :: Carta
+reversaVerde = CartaEspecial Reversa Verde
+
+reversaRojo :: Carta
+reversaRojo = CartaEspecial Reversa Rojo
+
+saltarTurnoRojo :: Carta
+saltarTurnoRojo = CartaEspecial SaltarTurno Rojo
+
+mas4Rojo :: Carta
+mas4Rojo = CartaEspecial Mas4 Rojo
+
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "Tests de clase 3" $ do
-    it "" $ do
-      2 + 2 `shouldBe` 4
+    it "una carta se puede jugar despues de otra si coinciden sus numeros" $ do
+      sePuedeJugar ochoAzul ochoRojo `shouldBe` True
+    it "una carta no se puede jugar despues de otra si no coinciden sus numeros ni sus colores" $ do
+      sePuedeJugar ochoAzul sieteVerde `shouldBe` False
+    it "una carta se puede jugar despues de otra si coinciden sus colores" $ do
+      sePuedeJugar ochoAzul sieteAzul `shouldBe` True
+    it "una carta especial se puede jugar sobre otra carta especial si tiene la misma figura" $ do
+      sePuedeJugar reversaVerde reversaRojo `shouldBe` True
+    it "una carta especial se puede jugar sobre otra carta especial si tiene el mismo color" $ do
+      sePuedeJugar reversaRojo saltarTurnoRojo `shouldBe` True
+    it "una carta especial no se puede jugar sobre otra carta especial si no tiene misma figura ni mismo color" $ do
+      sePuedeJugar reversaVerde saltarTurnoRojo `shouldBe` False
+    it "siempre se puede jugar una carta de mas 4 por sobre otra carta" $ do
+      sePuedeJugar sieteAzul mas4Rojo `shouldBe` True
+    it "puedo jugar una carta sobre un mas 4 solo si es del color del mas 4" $ do
+      sePuedeJugar mas4Rojo reversaRojo `shouldBe` True
+    it "no puedo jugar una carta sobre un mas 4 si es de diferente color que el mas 4" $ do
+      sePuedeJugar mas4Rojo sieteAzul `shouldBe` False

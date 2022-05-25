c7fac13ae2701e722e0f1e5997756f7b4bb3d637
T-J-Antonio <tantonio@frba.utn.edu.ar>
2022-05-25 01:32:02 -0300

    movidos estilos a CSS

--- clase-bonus/src/Main.elm
+++ clase-bonus/src/Main.elm
@@ -4,8 +4,8 @@ module Main exposing (..)
 import Browser
 import Html exposing (Html, button, div, text, span, input)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (style)
-import String exposing (length, fromChar)
+import Html.Attributes exposing (style, class)
+import String exposing (length, fromChar, fromInt)
 import List exposing (map)
 import VerificarPalabra exposing (..)
 
@@ -52,49 +52,47 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     CambiarPalabra str ->
-      if length str > 5 then model else {
-        palabra = str,
-        intento = "",
-        resultado = [],
-        cantIntentos = model.cantIntentos}
-    ChequearPalabra -> case (model.cantIntentos - 1) > 0 of
+      if length str > 5 then model else 
+        { palabra = str
+        , intento = ""
+        , resultado = model.resultado
+        , cantIntentos = model.cantIntentos }
+    ChequearPalabra -> case model.cantIntentos > 0 of
       True ->    
-        { palabra = model.palabra,
-        intento = model.intento,
-        resultado = chequear model.palabra model.intento,
-        cantIntentos = model.cantIntentos - 1 }
+        { palabra = model.palabra
+        , intento = model.intento
+        , resultado = chequear model.palabra model.intento
+        , cantIntentos = model.cantIntentos - 1 }
       False ->    
-        { palabra = model.palabra,
-        intento = model.intento,
-        resultado = model.resultado,
-        cantIntentos = 0 }
+        { palabra = model.palabra
+        , intento = model.intento
+        , resultado = model.resultado
+        , cantIntentos = 0 }
     CambiarIntento str ->
-      { palabra = model.palabra,
-        intento = str,
-        resultado = [],
-        cantIntentos = model.cantIntentos}
+      { palabra = model.palabra
+      , intento = str
+      , resultado = []
+      , cantIntentos = model.cantIntentos }
      
 
 
 -- VIEW
 
--- mover el style a CSS (¿o no? ¿qué queda más funcional?)
-
 view : Model -> Html Msg
 view model =
   div 
-  [ style "display" "flex", style "flex-direction" "column" ] [
+  [ class "padre" ] [
     input [ onInput CambiarPalabra ] []
     , input [ onInput CambiarIntento ] []
     , button [ onClick ChequearPalabra ] [ text "chequear" ]
-    , div [ style "display" "flex", style "flex-direction" "row" ] (map vistaResultado model.resultado) 
+    , span [] [ text ("intentos restantes: " ++ fromInt model.cantIntentos) ]
+    , div [ class "contenedorCuadraditos" ] (map vistaResultado model.resultado) 
   ]
 
 vistaResultado : Verificacion -> Html Msg
 vistaResultado unaVerificacion = div 
-  [ style "background-color" (obtenerColor unaVerificacion)
-  , style "height" "100px"
-  , style "width" "100px" ] 
+  [ class "cuadradito" 
+  , style "background-color" (obtenerColor unaVerificacion) ] 
   [ text (fromChar unaVerificacion.letra) ]
 
 obtenerColor : Verificacion -> String
